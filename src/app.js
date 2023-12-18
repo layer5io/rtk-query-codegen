7468da331c2e8a7c919a31a1b498a5d1b9e02c8f
aabidsofi19 <mailtoaabid01@gmail.com>
2023-12-18 16:33:52 +0530

    remove unnecessary files
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
-const { program } = require('commander');
-const { exec } = require('child_process');
-const { renameSync, rmdirSync, unlinkSync } = require('fs');
-const { dirname, basename, join } = require('path');
+#! /usr/bin/env node
+var { program } = require("commander");
+var { exec } = require("child_process");
+var { renameSync, rmdirSync, unlinkSync } = require("fs");
+var { dirname, basename, join } = require("path");
+var fs = require("fs");
+var ts = require("typescript");
+var compileConfig = require("./apiCompilerConfig.json");
 
 program
-    .option('-i, --input <input>', 'Input YAML schema file path')
-    .option('-o, --output <output>', 'Output path including the filename (e.g., /output/api.js)')
+    .option("-i, --input <input>", "Input YAML schema file path")
+    .option(
+        "-o, --output <output>",
+        "Output path including the filename (e.g., /output/api.js)"
+    )
+    .option("-c, --config <config>", "Config file path (e.g., /config.json)")
     .parse(process.argv);
+
+const compileApi = (tsPath, outputPath) => {
+    // read ts file
+    const tsFile = fs.readFileSync(tsPath, "utf8");
+    const result = ts.transpile(tsFile, compileConfig);
+    // write the result to output file
+    fs.writeFileSync(outputPath, result);
+    console.log("wrote output to " + outputPath);
+};
 
 // Function to run a command and handle success or failure
 function runCommand(command, successMessage, failureMessage) {
@@ -24,42 +41,44 @@ function runCommand(command, successMessage, failureMessage) {
     });
 }
 
-async function generateAPI(inputFilePath, outputFilePath) {
+async function generateAPI(inputFilePath, outputFilePath, config) {
     try {
         // Convert YAML schema to JSON schema
         const yamlToJSONCommand = `redocly bundle ${inputFilePath} -o ./openapi.json --ext json`;
-        await runCommand(yamlToJSONCommand, 'JSON bundle generation successful', 'JSON bundle generation failed');
+        await runCommand(
+            yamlToJSONCommand,
+            "JSON bundle generation successful",
+            "JSON bundle generation failed"
+        );
 
         // Run OpenAPI generator
-        const openApiGeneratorCommand = 'npx @rtk-query/codegen-openapi openapi-config.ts';
-        await runCommand(openApiGeneratorCommand, 'OpenAPI generation successful', 'OpenAPI generation failed');
+        const openApiGeneratorCommand = `npx @rtk-query/codegen-openapi ${config}`;
+        await runCommand(
+            openApiGeneratorCommand,
+            "OpenAPI generation successful",
+            "OpenAPI generation failed"
+        );
 
         // Run TypeScript compilation
-        const tscCommand = 'tsc --build tsconfig.json';
-        await runCommand(tscCommand, 'TypeScript compilation successful', 'TypeScript compilation failed');
+        compileApi("./api.ts", outputFilePath);
 
-        // Move api.js from the generated folder to the output path
-        console.log('Removing Build Artifacts');
-        const outputPath = dirname(outputFilePath);
-        const outputFilename = basename(outputFilePath);
-        renameSync('./dist/api.js', join(outputPath, outputFilename));
-        rmdirSync('./dist', { recursive: true });
-        unlinkSync('./openapi.json');
-        unlinkSync('./api.ts');
+        console.log("Removing Build Artifacts");
+        unlinkSync("./openapi.json");
+        unlinkSync("./api.ts");
 
-        console.log('API generation successful');
+        console.log("API generation successful");
         process.exit(0);
     } catch (error) {
-        console.error('API generation failed');
+        console.error("API generation failed", error);
         process.exit(1);
     }
 }
 
-const { input, output } = program.opts();
+const { input, output, config } = program.opts();
 
-if (!input || !output) {
-    console.error('Please provide both input and output options.');
+if (!input || !output || !config) {
+    console.error("Please provide input, output, and config paths");
     process.exit(1);
 }
 
-generateAPI(input, output);
+generateAPI(input, output, config);
